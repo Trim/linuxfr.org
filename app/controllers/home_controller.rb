fe8f161d05a2f4fab639bfc9037e50616721b463
Adrien Dorsaz <adrien@adorsaz.ch>
2020-07-20 14:18:02 +0200

    home: split home from default application layout and implements first new nav bandeau

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,6 @@
 # encoding: utf-8
 class HomeController < ApplicationController
-  DEFAULT_TYPES = %w(News Poll)
-
   def index
-    @sections = Section.published
-    @types  = current_account.try(:types_on_home)
-    @types  = DEFAULT_TYPES if @types.blank?
-    default = current_account.try(:sort_by_date_on_home) ? "created_at" : "interest"
-    @order  = params[:order]
-    @order  = default unless VALID_ORDERS.include?(@order)
-    @ppp    = Node.ppp
-    @banner = Banner.random
-    @poll   = Poll.current
-    @nodes  = Node.public_listing(@types, @order).page(params[:page])
+    render :index, layout: 'home'
   end
 end
