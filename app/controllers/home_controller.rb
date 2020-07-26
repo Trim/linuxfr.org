7efd7620538a4cb5d8c50631d125b385bbb58365
Adrien Dorsaz <adrien@adorsaz.ch>
2020-07-26 16:15:51 +0200

    home: add diaries (with full body at the moment)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 class HomeController < ApplicationController
   def index
     @news  = Node.public_listing(News, "created_at").limit(5)
+    @diaries = Node.public_listing(Diary, "created_at").limit(5)
     @banner = Banner.random
     render :index, layout: 'home'
   end
