a7208976e0ea97d2f72b9089ee71a67438eee69d
Adrien Dorsaz <adrien@adorsaz.ch>
2020-07-27 23:09:25 +0200

    home: add posts, poll, bookmarks, wiki_pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
     @news = Node.home_listing(News)
     @diaries = Node.home_listing(Diary)
+    @posts = Node.home_listing(Post)
+    @polls = Node.home_listing(Poll).limit(1)
+    @bookmarks = Node.home_listing(Bookmark)
+    @wiki_pages = Node.home_listing(WikiPage)
     @banner = Banner.random
   end
 end
